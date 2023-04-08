e1a6b858439ffb3101780d79fb009cd367b3bf0b
Michael J Sigg <mjsigg@gmail.com>
2023-04-07 23:18:44 -0500

    loader added

--- nextfire-app/.next/static/chunks/webpack.js
+++ nextfire-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c22c917dfdc4693"; }
+/******/ 		__webpack_require__.h = function() { return "5be9a90092736b0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- nextfire-app/src/pages/index.tsx
+++ nextfire-app/src/pages/index.tsx
@@ -2,14 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
 
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch = {false} href={{
-        pathname: "/[username]",
-        query: {username: "smbdy"},
-      }}></Link>
+        <Loader show/>
     </div>
-  )
+  );
 }
