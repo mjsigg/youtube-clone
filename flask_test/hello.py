4902ebfc6930222f5bb42e9fc18289869eabf8bb
Michael J Sigg <mjsigg@gmail.com>
2023-04-11 14:08:07 -0500

    init

---
+++ flask_test/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = "michael"
+    
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}"
+
+
