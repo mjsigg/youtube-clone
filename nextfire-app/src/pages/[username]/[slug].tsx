24fea3c33bf692535e753d1d5e06ce2b871b0b2e
Michael J Sigg <mjsigg@gmail.com>
2023-07-23 23:09:49 -0500

    deleting nextfireapp

--- nextfire-app/src/pages/[username]/[slug].tsx
+++
@@ -1,7 +0,0 @@
-export default function PostPage({}) {
-    return (
-        <main>
-            <h1>Sign up</h1>
-        </main>
-    )
-}
--- nextfire-app/src/pages/admin/[slug].js
+++
@@ -1,7 +0,0 @@
-export default function AdminPostEdit({}) {
-    return (
-        <main>
-            <h1>Sign up</h1>
-        </main>
-    )
-}
